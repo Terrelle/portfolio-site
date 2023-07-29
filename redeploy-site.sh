05171d59ba4e205e8bf7dd1fda11f52fa7b34d9f
Terrelle Tettey <terrelle@thegenzbuzz.com>
2023-07-29 13:55:16 -0400

    modified redeploy-site and removed hobbies from site

--- redeploy-site.sh
+++ redeploy-site.sh
@@ -1,15 +1,9 @@
 #!/bin/bash
 
-
 cd /root/portfolio-site
 
 git fetch && git reset origin/main --hard
 
-source python3venv/bin/activate
+docker compose -f docker-compose.prod.yml down
 
-
-pip install -r requirements.txt
-
-systemctl daemon-reload
-
-systemctl restart myportfolio
+docker compose -f docker-compose.prod.yml up -d --build
